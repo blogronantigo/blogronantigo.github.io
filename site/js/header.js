afa895ff22dbc2a45d4b4fbdba53a2c835b44706
Ron <79697100+blogronantigo@users.noreply.github.com>
2024-12-10 14:45:30 -0300

    Update header.js

--- site/js/header.js
+++ site/js/header.js
@@ -4,7 +4,7 @@ function showHeader()
     `
     <header>
         <nav>
-            <a href="https://rontekhne.github.io"><img src="/site/img/R.png" alt="logo R"></a>
+            <a href="https://blogronantigo.github.io"><img src="/site/img/R.png" alt="logo R"></a>
             <ul>
                 <li><a href="/site/src/about.html">SOBRE</a></li>
                 <li><a href="/post/posts.html">POSTAGENS</a></li>
