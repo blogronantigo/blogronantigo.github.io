027af00bcef331f7a299115449f994d9e3a1eb01
Ron <79697100+blogronantigo@users.noreply.github.com>
2024-12-10 14:43:52 -0300

    Update posts.js

--- site/js/posts.js
+++ site/js/posts.js
@@ -4,7 +4,7 @@ function showPosts()
     `
     <section>
         <article>
-        <img class="post-card-img" src="https://raw.githubusercontent.com/rontekhne/OitoP/main/app/src/main/res/mipmap-hdpi/oitop.png" width="100" height="100" alt="8p logo">
+        <img class="post-card-img" src="https://raw.githubusercontent.com/blogronantigo/OitoP/main/app/src/main/res/mipmap-hdpi/oitop.png" width="100" height="100" alt="8p logo">
             <h2>OITOP</h2>
             <p> 
                 O quebra-cabeças de 8 peças, como o nome diz, possui 8 peças, 
@@ -15,7 +15,7 @@ function showPosts()
             <a class="read" href="/post/02-01-2024_oitop.html">LER</a>
         </article>
         <article>
-        <img class="post-card-img" src="https://raw.githubusercontent.com/rontekhne/Arx/main/app/src/main/res/mipmap-hdpi/arx_icon.png" width="100" height="100" alt="Arx logo">
+        <img class="post-card-img" src="https://raw.githubusercontent.com/blogronantigo/Arx/main/app/src/main/res/mipmap-hdpi/arx_icon.png" width="100" height="100" alt="Arx logo">
             <h2>ARX ARCADE 2D</h2>
             <p> 
                 Os arxes roubaram as almas da galáxia. Você como 
@@ -26,7 +26,7 @@ function showPosts()
             <a class="read" href="/post/23-09-2023_arx.html">LER</a>
         </article>
         <article>
-        <img class="post-card-img" src="https://raw.githubusercontent.com/rontekhne/rontekhne.github.io/main/site/img/R.png" width="100" height="100" alt="RonTekhne logo">
+        <img class="post-card-img" src="https://raw.githubusercontent.com/blogronantigo/blogronantigo.github.io/main/site/img/R.png" width="100" height="100" alt="RonTekhne logo">
             <h2>RONTEKHNE PARA ANDROID</h2>
             <p>
                 O blog está no Android. Oba! Leve o  RonTekhne 
